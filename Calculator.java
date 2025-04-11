a1cf91e75d156449d6cb5f4907856e290e870ffa
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-04-11 12:16:57 +0530

    Subtraction function added to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
     public static int add(int a,int b){
         return a+b;
     }
+    public static int sub(int a,int b){
+        return a-b;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,7 +14,9 @@ public class Calculator {
         int a=sc.nextInt();
         int b=sc.nextInt();
         int sum=add(a, b);
+        int diff=sub(a,b);
         System.out.println("The sum is:"+sum);
+        System.out.println("The difference is:"diff);
 
 
     }
