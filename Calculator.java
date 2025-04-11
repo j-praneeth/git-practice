91e7ad825ed0e7cf05cdf53f2b67d0c3c880b000
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-04-11 12:13:31 +0530

    Addition function added to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
 public class Calculator {
+    public static int add(int a,int b){
+        return a+b;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the values of a & b:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=add(a, b);
+        System.out.println("The sum is:"+sum);
+
+
     }
 }
