489acbad295b4767322d1b17195a34ef8447d5b9
Varshini052004 <varshinikodurupaka@gmail.com>
2025-04-11 12:32:39 +0530

    Update mod function Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -14,6 +14,10 @@ public class Calculator {
     public static int div(int a, int b) {
         return a / b;
     }
+    public static int mod(int a, int b) {
+        return a % b;
+    }
+    
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -24,10 +28,12 @@ public class Calculator {
         int diff=sub(a,b);
         int prod = prod(a,b);
         int div = div(a,b);
+        int mod = mod(a,b);
         System.out.println("The sum is:"+sum);
         System.out.println("The diff is:"+diff);
         System.out.println("The product is: "+prod);
         System.out.println("The division is: "+div);
+        System.out.println("The mod is:"+mod);
 
 
     }
