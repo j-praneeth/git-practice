c012b3fc8df174a1805ae10715ebfb5a6a0852c4
Sai Teertha Sri Inavolu <153907775+teertha3@users.noreply.github.com>
2025-04-11 11:54:26 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
 public class Calculator {
+    public static int addition(int a, int b){
+        return a+b;
+
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a & b values:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum = addition(a, b);
+        System.out.println("The sum is: " + sum);
+
     }
 }
